f85a21730c1a9f511f30e2327fc1d2b7eb850bd9
Diego Cubillos <dcubillos12@hotmail.com>
2024-09-11 12:52:27 -0500

    se corrige ssl a false

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
-import { Module } from "@nestjs/common";
-import { UsersController } from "./users.controller";
-import { UsersService } from "./users.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "./entities/user.entity";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -13,14 +13,15 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: "postgres",
-        host: configService.get<string>("DB_HOST"),
-        port: configService.get<number>("DB_PORT") || 5432,
-        username: configService.get<string>("DB_USERNAME") || "postgres",
-        password: configService.get<string>("DB_PASSWORD") || "CubillosOrtiz1",
-        database: configService.get<string>("DB_NAME") || "usersMovies",
-        entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT') || 5432,
+        username: configService.get<string>('DB_USERNAME') || 'postgres',
+        password: configService.get<string>('DB_PASSWORD') || 'CubillosOrtiz1',
+        database: configService.get<string>('DB_NAME') || 'usersMovies',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
+        ssl: false,
       }),
     }),
     TypeOrmModule.forFeature([User]),
