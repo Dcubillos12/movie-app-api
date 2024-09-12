fa0df010a5e4ab7073689f511cfd922170aeba9d
Diego Cubillos <dcubillos12@hotmail.com>
2024-09-12 15:20:11 -0500

    configuracion de coors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept , Authorization',
+  });
+  
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(configService.get("PORT") || 3000);
+  await app.listen(configService.get('PORT') || 3000);
 }
 void bootstrap();
